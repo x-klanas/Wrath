5890c691f24e82c2dedbf3e42cc3a5795a2823ff
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2019-04-11 16:28:28 +0300

    #1 Moved MainMenu script to Scripts/Menu, removed Canvas from PlayScene

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu {
+    public class MainMenu : MonoBehaviour {
+        public void PlayGame() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame() {
+            Debug.Log("quit");
+            Application.Quit();
+        }
+    }
+}
