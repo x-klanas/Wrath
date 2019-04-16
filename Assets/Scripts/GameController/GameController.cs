e8f9d0c5a9bf9664d55b3756a0f359883ddc2367
Matas Gervė <48481421+matas1001@users.noreply.github.com>
2019-04-16 22:10:32 +0300

    User/39 added testing timer
    Testing timer added, matched with construction timer

--- Assets/Scripts/GameController/GameController.cs
+++ Assets/Scripts/GameController/GameController.cs
@@ -6,46 +6,73 @@ namespace GameController
     public class GameController : MonoBehaviour
     {
         public long constructionTime = 60;
-        private MyStopwatch timer = new MyStopwatch(DateTime.Now);
+        public long testingTime = 60;
+        private MyStopwatch constructionTimer = new MyStopwatch(DateTime.Now);
+        private MyStopwatch testingTimer = new MyStopwatch(DateTime.Now);
+        private bool constructionTimerTicking = true;
 
         // Start is called before the first frame update
         private void Start()
         {
-            StartTimer();
+            StartConstructionTimer();
         }
-
         // Update is called once per frame
         private void Update()
         {
-            CheckTimer();
-        }
-    
-        private void StartTimer()
-        {
-            timer.Reset();
-            timer.Start();
-        }
-
-        private void PauseTimer()
-        {
-            timer.Stop();
-        }
-
-        private void ResumeTimer()
-        {
-            timer.Start();
-        }
-
-        private void CheckTimer()
-        {
-            if (timer.IsRunning && timer.ElapsedMilliseconds >= constructionTime * 1000)
+            if (constructionTimerTicking)
             {
-                timer.Stop();
-                Debug.LogFormat("elapsed mm {0}", timer.ElapsedMilliseconds);
-                Debug.Log("Time is up");
+                CheckConstructionTimer();
+            }
+            else
+            {
+                CheckTestingTimer();
             }
         }
-    
-    
+
+
+
+        private void StartConstructionTimer()
+        {
+            constructionTimer.Reset();
+            constructionTimer.Start();
+        }
+        private void PauseTimer()
+        {
+            constructionTimer.Stop();
+        }
+        private void ResumeTimer()
+        {
+            constructionTimer.Start();
+        }
+        private void CheckConstructionTimer()
+        {
+            if (constructionTimer.IsRunning && constructionTimer.ElapsedMilliseconds >= constructionTime * 1000)
+            {
+                constructionTimer.Stop();
+                Debug.LogFormat("Constr elapsed mm {0}", constructionTimer.ElapsedMilliseconds);
+                Debug.Log("Time is up");
+                StartTestingTimer();
+                constructionTimerTicking = false;
+              
+            }
+        }
+
+        private void StartTestingTimer()
+        {
+            testingTimer.Reset();
+            testingTimer.Start();
+        }
+
+        private void CheckTestingTimer()
+        {
+            if (testingTimer.IsRunning && testingTimer.ElapsedMilliseconds >= testingTime * 1000)
+            {
+                testingTimer.Stop();
+                Debug.LogFormat("Testing elapsed mm {0}", testingTimer.ElapsedMilliseconds);
+                Debug.Log("Time is up");
+                //TeleportToEndGameArea();
+            }
+        }
+
     }
 }
