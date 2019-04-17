69e2f9014c2e7586af37f1402c65aaee3abdb9de
Simonas <simonas_ivanovas@yahoo.com>
2019-04-17 11:33:30 +0300

    #3 Teleportation in progress
    Working on a teleportation script for Resume button.

--- Assets/InGame_Menu.cs
+++ Assets/InGame_Menu.cs
@@ -2,12 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
 public class InGame_Menu : MonoBehaviour
 {
+    public Transform canvas;
+
+    public Vector3 old_position;//position before teleport
+    public Vector3 center;
+    public GameObject Player;//player
+    public GameObject Target; //teleportation
+
     // Start is called before the first frame update
     public void RestartGame()
     {
-        SceneManager.LoadScene("Temp_GameScene");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
+
+    public void Star()
+    {
+        old_position = new Vector3(Player.gameObject.transform.position.x, Player.gameObject.transform.position.y, Player.gameObject.transform.position.z);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                Player.gameObject.transform.position = Target.gameObject.transform.position;
+            }
+            
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Player.gameObject.transform.position = center;
+                //Player.gameObject.transform.position = old_position;
+                Time.timeScale = 1;
+            }
+        }
+    }
+    
+    
 }
