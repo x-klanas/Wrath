2febb9774b83805b60c69c4062a701730af50216
Simonas <simonas_ivanovas@yahoo.com>
2019-05-05 15:37:17 +0300

    #43 Created EndGame Area and an Exit menu
    #43
    Created an EndGame Area canvas
    Created an EndGame Area menu
    Created an Exit button
    Created an Exit button script - when clicked, button changes scene to Main Menu

---
+++ Assets/Scripts/Menu/EndGameArea_Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameArea_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform canvas;  // end game area canvas
+    public string mainMenuSceneName;// main menu scene name
+    
+    void Start()
+    {
+        
+    }
+
+    public void ExitButton()
+    {   
+        canvas.gameObject.SetActive(false);
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
